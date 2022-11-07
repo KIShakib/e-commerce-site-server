2f443b3f51db6a7c6ae19d013ed04f88ee7e6b49
Khubaibul Islam <khubaibulislam2001@gmail.com>
2022-11-08 03:05:58 +0600

    Foods Add In Mongodb are completed By Using POST Method

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const express = require('express');;
+const express = require('express');
 const cors = require('cors');
 const jwt = require("jsonwebtoken");
 require("colors");
-require('colors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -19,12 +18,37 @@ app.get("/", (req, res) => {
 });
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mogodb-practice.uoisaxb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// Database Function
 
+async function run() {
+    try {
+        const foodCollection = client.db("shakibs-kitchen").collection("foods");
 
+        app.post("/addfood", async (req, res) => {
+            const food = req.body;
+            const result = await foodCollection.insertOne(food);
+            res.send(result)
+        })
 
+        app.get("/homepagefoods", async (req, res) => {
+            const query = {};
+            const cursor = foodCollection.find(query);
+            const foods = await cursor.limit(3).toArray();
+            res.send(foods)
+        })
 
-
+    }
+    catch (error) {
+        // console.log((error.name).bgRed);
+        // console.log((error.message).bgCyan.bold);
+        // console.log((error.stack).bgRed.bold);
+        console.log(error);
+    }
+}
+run()
 
 
 
@@ -33,5 +57,5 @@ app.get("/", (req, res) => {
 
 // Listen
 app.listen(port, () => {
-    console.log(`Server Is Running On ${port}`.bgMagenta.bold);
+    console.log(`Server Is Running On ${port}`.bgCyan.bold);
 })
