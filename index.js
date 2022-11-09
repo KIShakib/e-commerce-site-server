5d015454a4570695c132df7f9bbe1765d2ff9658
Khubaibul Islam <khubaibulislam2001@gmail.com>
2022-11-09 12:33:46 +0600

    Review by specific id/food API by date sorting complete

--- index.js
+++ index.js
@@ -26,25 +26,55 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const foodCollection = client.db("shakibs-kitchen").collection("foods");
+        const reviewCollection = client.db("shakibs-kitchen").collection("reviews");
 
+        // Add Food From Admin DashBoard
         app.post("/addfood", async (req, res) => {
             const food = req.body;
             const result = await foodCollection.insertOne(food);
             res.send(result)
         })
 
+        // Send Limited Food For Homepage
         app.get("/homepagefoods", async (req, res) => {
             const query = {};
             const cursor = foodCollection.find(query);
-            const foods = await cursor.limit(3).toArray();
+            const foods = await cursor.sort({ addedTimeEncrypted: -1 }).limit(3).toArray();
             res.send(foods)
         })
+
+        // Send All Food For Foods Page
         app.get("/foods", async (req, res) => {
             const query = {};
             const cursor = foodCollection.find(query);
-            const foods = await cursor.toArray();
+            const foods = await cursor.sort({ addedTimeEncrypted: -1 }).toArray();
             res.send(foods)
         })
+
+        // Send Food By Id For Individual Food Details
+        app.get("/food/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const food = await foodCollection.findOne(query);
+            res.send(food);
+        })
+
+        // Add Reviews
+        app.post("/addreview", async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result)
+        })
+
+        // Review By Id
+        app.get("/reviewbyid:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { foodId: id };
+            const cursor = reviewCollection.find(query);
+            const reviewById = await cursor.sort({ addedTimeEncrypted: -1 }).toArray();
+            res.send(reviewById)
+        })
+
 
     }
     catch (error) {
